eac266b3c034379be2ac2d307e0c8cb4386c9d52
David Lemvigh <dlemvigh@gmail.com>
2016-11-23 21:26:46 +0100

    sandbox

---
+++ client/Components/Sandbox/Sandbox.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Relay from 'react-relay'
+
+import Activity from '../Activity/Activity'
+import Leaderboard from '../Leaderboard/Leaderboard'
+
+class Sandbox extends React.Component {
+    render() {
+        return (
+            <div>
+                Sandbox
+                <Leaderboard store={this.props.store} />
+                <Activity store={this.props.store} user={this.props.user} />
+            </div>
+        );
+    }
+}
+
+Sandbox = Relay.createContainer(Sandbox, {
+    fragments: {
+        store: () => Relay.QL`
+            fragment on Store {
+                ${Leaderboard.getFragment('store')}
+                ${Activity.getFragment('store')}
+            }
+        `,
+        user: () => Relay.QL`
+            fragment on User {
+                ${Activity.getFragment('user')}
+            }
+        `
+    }
+});
+
+export default Sandbox;
--- client/routes.js
+++ client/routes.js
@@ -7,6 +7,8 @@ import Activity from './Components/Activity/Activity'
 import ActivityQueries from './Components/Activity/ActivityQueries'
 import Leaderboard from './Components/Leaderboard/Leaderboard'
 import LeaderboardQueries from './Components/Leaderboard/LeaderboardQueries'
+import Sandbox from './Components/Sandbox/Sandbox'
+import SandboxQueries from './Components/Sandbox/SandboxQueries'
 
 const Routes = (props) => ( 
   <Router {...props}>
@@ -14,12 +16,20 @@ const Routes = (props) => (
       <IndexRoute  
         component={Leaderboard}
         queries={LeaderboardQueries}
-        render={({props}) => props && <Leaderboard {...props} /> } />
+        render={({props}) => props && <Leaderboard {...props} /> } 
+        />
+      <Route
+        path='/sandbox'
+        component={Sandbox}
+        queries={SandboxQueries }
+        render={({props}) => props && <Sandbox {...props} />}
+        />
       <Route 
         path='/:username' 
         component={Activity} 
         queries={ActivityQueries}
-        render={({props}) => props && <Activity {...props} />} />  
+        render={({props}) => props && <Activity {...props} />} 
+        />  
     </Route>
   </Router>
 );
