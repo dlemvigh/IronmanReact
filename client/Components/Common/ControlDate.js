cc81889ca795d1fdd84d72722f53400eef4c7dcb
David Lemvigh <david.lemvigh@gmail.com>
2016-12-05 12:41:36 +0100

    mobile-detect to use native date-picker
    attempt at switching rendering of date-picker based on mobile browser

--- client/Components/Common/ControlDate.js
+++ client/Components/Common/ControlDate.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { ControlLabel, FormGroup } from "react-bootstrap";
 import DateTime from "react-datetime";
+import MobileDetect from "mobile-detect";
 import moment from "moment";
 
 export default class ControlDate extends React.Component {
+
+  isMobile(){
+    const md = new MobileDetect(window.navigator.userAgent);
+    return !!md.mobile();
+  }
 
   isValid(){
     return this.getValidationState() === "success";
@@ -17,7 +23,21 @@ export default class ControlDate extends React.Component {
     }
   }
 
+  getValue = () => {
+    debugger;
+    return this.props.value.format("YYYY-MM-DD");
+  }
+
+  onChangeMobile = (event) => {
+    if (this.props.onChange) {
+      const m = new moment(event.target.value);
+      const value = m.startOf("day");
+      this.props.onChange(value);
+    }
+  }
+
   onChange = (value) => {
+    debugger
     if (typeof value !== "string") {
       value = value.startOf("day");
     }
@@ -30,13 +50,19 @@ export default class ControlDate extends React.Component {
     return (
       <FormGroup validationState={this.getValidationState()}>
         <ControlLabel>Date</ControlLabel>
-        <DateTime
-          dateFormat="D/M-YYYY"
-          timeFormat={false}
-          defaultValue={moment.utc()}
-          value={this.props.value}
-          onChange={this.onChange}
-        />
+        {
+          this.isMobile() 
+          ? 
+          <input type="date" className="form-control" value={this.getValue()} onChange={this.onChangeMobile} />
+          :
+          <DateTime
+            dateFormat="D/M-YYYY"
+            timeFormat={false}
+            defaultValue={moment.utc()}
+            value={this.props.value}
+            onChange={this.onChange}
+          />
+        }
       </FormGroup>
     );
   }
