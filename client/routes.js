c289ba6941469de846ff62d097d14efc30c6b955
David Lemvigh <david.lemvigh@gmail.com>
2017-01-27 12:54:09 +0100

    loading bar

--- client/routes.js
+++ client/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Router, Route, IndexRoute } from "react-router";
+
 import App from "./Components/App/App";
 import AppQueries from "./Components/App/AppQueries";
 import Activity from "./Components/Activity/Activity";
@@ -9,31 +10,33 @@ import LeaderboardQueries from "./Components/Leaderboard/LeaderboardQueries";
 import Sandbox from "./Components/Sandbox/Sandbox";
 import SandboxQueries from "./Components/Sandbox/SandboxQueries";
 
+import Loading from "./Components/Common/Loading";
+
 const Routes = (props) => ( 
   <Router {...props}>
     <Route path="/" component={App} queries={AppQueries}>
       <IndexRoute  
         component={Leaderboard}
         queries={LeaderboardQueries}
-        render={({props}) => props && <Leaderboard {...props} />} 
+        render={({props}) => props ? <Leaderboard {...props} /> : <Loading show={true} />} 
       />
       <Route
         path="/sandbox"
         component={Sandbox}
         queries={SandboxQueries}
-        render={({props}) => props && <Sandbox {...props} />}
+        render={({props}) => props ? <Sandbox {...props} /> : <Loading show={true} />}
       />
       <Route 
         path="/user/:id" 
         component={Activity} 
         queries={ActivityQueries.byId}
-        render={({props}) => props && <Activity {...props} />} 
+        render={({props}) => props ? <Activity {...props} /> : <Loading show={true} />} 
       />  
       <Route 
         path="/:username" 
         component={Activity} 
         queries={ActivityQueries.byUsername}
-        render={({props}) => props && <Activity {...props} />} 
+        render={({props}) => props ? <Activity {...props} /> : <Loading show={true} />} 
       />  
     </Route>
   </Router>
