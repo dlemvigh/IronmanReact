a8686902fecc8fc37208d0ceed8de66d802b7aac
David Lemvigh <dlemvigh@gmail.com>
2017-10-01 00:47:31 +0200

    a bit of cleanup

--- shared/config.js
+++ shared/config.js
@@ -1,38 +1,41 @@
 const base = {
-    db: "ironman",
+  db: "ironman",
 };
 
 export const prod = {
-    ...base,
-    port: 4000
-}
+  ...base,
+  port: 4000, 
+  callbackUrl: "https://ironman.dlemvigh.dk/callback"
+};
 
 export const test = {
-    ...base,
-    db: "test",
-    port: 4001
-}
+  ...base,
+  db: "test",
+  port: 4001,
+  callbackUrl: "https://test.ironman.dlemvigh.dk/callback"
+};
 
 export const dev = {
-    ...base,
-    port: 8080
-}
+  ...base,
+  port: 8080,
+  callbackUrl: "http://localhost:8080/callback"
+};
 
-export const config = { prod, test, dev }
+export const config = { prod, test, dev };
 
 export function getEnv() {
-    switch(process.env.NODE_ENV) {
-        case "development":
-            return "dev";
-        case "test":
-            return "test";
-        case "production":
-        default:
-            return "prod";
-    }
+  switch(process.env.NODE_ENV) {
+    case "development":
+      return "dev";
+    case "test":
+      return "test";
+    case "production":
+    default:
+      return "prod";
+  }
 }
 
 export function getConfig() {
-    const env = getEnv();
-    return config[env];
+  const env = getEnv();
+  return config[env];
 }
