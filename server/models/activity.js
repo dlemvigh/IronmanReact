c45685a147ccbbdf08e773b4c9f7f7bb76ae75e2
David Lemvigh <dlemvigh@gmail.com>
2019-06-03 00:09:24 +0200

    fix lint errors

--- server/models/activity.js
+++ server/models/activity.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const moment = require("moment");
 const activitySchema = new mongoose.Schema({
   userId: {
--- server/models/discipline.js
+++ server/models/discipline.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const disciplineSchema = new mongoose.Schema({
   name: {
     type: String,
