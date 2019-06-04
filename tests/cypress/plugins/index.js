28d5e3ed4495ba7d7f403ad68db32dd57470612d
David Lemvigh <dlemvigh@gmail.com>
2019-06-04 23:13:48 +0200

    cypress environment config

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -11,7 +11,21 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const fs = require("fs-extra");
+const path = require("path");
+
+function getConfigFile(env) {
+  const file = path.resolve("cypress", "config", `cypress.${env}.json`);
+  console.log("file", file);
+  return fs.readJson(file);
+}
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  console.log("config", config.env);
+
+  if (config.env.configFile) {
+    return getConfigFile(config.env.configFile);
+  }
 }
